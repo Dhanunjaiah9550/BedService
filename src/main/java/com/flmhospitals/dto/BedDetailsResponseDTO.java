6877ddb19dddb7538df62726eab88343d220537c
Fayaz <kurubanethravathi2@gmail.com>
2025-10-26 16:22:59 +0530

    As per comments made changes in BedDetailsResponseDTO and BedRequestDTO

--- src/main/java/com/flmhospitals/dto/BedDetailsResponseDTO.java
+++ src/main/java/com/flmhospitals/dto/BedDetailsResponseDTO.java
@@ -1,7 +1,5 @@
 package com.flmhospitals.dto;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,12 +13,9 @@ public class BedDetailsResponseDTO {
 	
 	private int bedNum;
 	
-//	private RoomDetailsResponseDTO roomDetailsResponseDTO;
+	private long roomNumber;
 	
 	private boolean isOccupied;
 	
-	private String patientId;
-	
-	private List<BedAssignemntHistoryDTO> bedAssignemntHistorydto;
 
 }
--- src/main/java/com/flmhospitals/dto/BedRequestDTO.java
+++ src/main/java/com/flmhospitals/dto/BedRequestDTO.java
@@ -1,7 +1,5 @@
 package com.flmhospitals.dto;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +11,8 @@ public class BedRequestDTO {
 	
 	private int bedNum;
 	
-//	private RoomRequestDto roomRequestDto;
+	private long roomNumber;
 	
 	private boolean isOccupied;
 	
-	private String patientId;
-	
-	private List<BedAssignemntHistoryDTO> bedAssignemntHistorydto;
-
 }
