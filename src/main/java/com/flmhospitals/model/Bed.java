b10f2da217a029ba72f7388fa75daf6eb79643d0
aravind-boddula <boddulaaravind2017@gmail.com>
2025-10-28 12:09:34 +0530

    Added BedDtoBuilder class and created a static method which will convert Bed entity to BedDetailsResponseDTO

--- src/main/java/com/flmhospitals/model/Bed.java
+++ src/main/java/com/flmhospitals/model/Bed.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -32,18 +33,22 @@ public class Bed {
 	
 	private long patientId;
 	
+	@ManyToOne
+    @JoinColumn(name = "roomId")
+	private Room roomId;
+	
 	@OneToMany(mappedBy = "bed",cascade = CascadeType.ALL)
 	private List<BedAssignmentHistory> bedAssignmentHistoryList;
 
-	public Bed(long bedNumber, boolean isOccupied, long patientId,
+	public Bed(long bedNumber, boolean isOccupied, long patientId, Room roomId,
 			List<BedAssignmentHistory> bedAssignmentHistoryList) {
 		super();
 		this.bedNumber = bedNumber;
 		this.isOccupied = isOccupied;
 		this.patientId = patientId;
+		this.roomId = roomId;
 		this.bedAssignmentHistoryList = bedAssignmentHistoryList;
 	}
-	
-	
+
 
 }
