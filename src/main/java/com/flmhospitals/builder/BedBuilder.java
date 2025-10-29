c918515d7852f7d1d3125163c715522c18d0127d
SAI <saihemanth@gmail.com$ git config --global user.email saihemanth@gmail.com$ git config --global user.email saihemanth915@gmail.com>
2025-10-29 22:56:56 +0530

    Add changes

--- src/main/java/com/flmhospitals/builder/BedBuilder.java
+++ src/main/java/com/flmhospitals/builder/BedBuilder.java
@@ -1,5 +1,14 @@
 package com.flmhospitals.builder;
 
+import com.flmhospitals.dto.BedRequestDTO;
+import com.flmhospitals.model.Bed;
+
 public class BedBuilder {
 
+	public static Bed buildRequestDtoFromBed(BedRequestDTO bedRequestDTO) {
+		return Bed.builder()
+				  .bedNumber(bedRequestDTO.getBedNum())
+				  .isOccupied(bedRequestDTO.isOccupied())
+				  .build();
+	}
 }
